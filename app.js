1b2d887b628a47387cc1a92a24f4cf6e6cf5c4d9
Yunya Hsu <yunyahsu110@gmail.com>
2022-05-29 12:11:52 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,36 @@
+// 引入外部套件
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+
+// 建立基本參數
+const port = 3000
+const app = express()
+
+// 使用express-handlebars為樣板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// 使用body-parser和method-override
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// 建立mongoose連線
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () =>
+  console.log('Error! mongoDB connect error.'))
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
+
+// 設定路由
+app.get('/', (req, res) => {
+  res.send('you got it')
+})
+
+// 監聽路由
+app.listen(port, () => {
+  console.log(`Express is running on localhost:${port}`)
+})
